6ee97a4bc9a4552776dcef2b2bf83b9ef980e7d3
Frankie Robertson <frankie@robertson.name>
2017-05-28 11:47:21 +0200

    Add factored training mode, major pos only modes and normalise scores

--- tagging.py
+++ tagging.py
@@ -6,24 +6,36 @@ import sys
 import pexpect
 
 
-def make_lattice(stagger_analysis):
+def line_to_factors(line_bits, include_minor=True):
+    minor_pos = line_bits[4] + "." + line_bits[5].replace("|", ".")
+    factors = [line_bits[1], line_bits[2], line_bits[4]]
+    if include_minor:
+        factors.append(minor_pos)
+    return '|'.join(factors)
+
+
+def proc_stagger_out(stagger_out):
+    stagger_out = stagger_out.decode("utf-8")
+    stagger_out = stagger_out.splitlines()
+    return (
+        line.rstrip('\n').split('\t')
+        for line in stagger_out)
+
+
+def lattice_sent(stagger_analysis, include_minor=True):
     # Make a dictionary of all edges and their scores for one analysis
     analyses = {}
-    stagger_analysis = stagger_analysis.decode("utf-8")
-    stagger_analysis = stagger_analysis.splitlines()
+    stagger_analysis = proc_stagger_out(stagger_analysis)
     in_analysis = True
     keys = []
     to_sum = []
 
     # Add new word analyses to the dictionary
     for line in stagger_analysis:
-        line = line.rstrip('\n')
-        line = line.split('\t')
         if len(line) == 1:
             in_analysis = False
         if in_analysis:
-            line[5] = line[4] + "." + line[5].replace("|", ".")
-            key = ''.join([line[1], "|", line[2], "|", line[4], "|", line[5]])
+            key = line_to_factors(line, include_minor=include_minor)
             to_sum.append(key)
             if key not in keys:
                 keys.append(key)
@@ -50,30 +62,79 @@ def make_lattice(stagger_analysis):
     for i in range(1, len(analyses)+1):
         i = str(i)
         lattice += "("
+        total = sum(analyses[i].values())
         for edge in analyses[i]:
-            score = str(analyses[i][edge])
+            score = str(analyses[i][edge] / total)
             lattice_edge = "(" + repr(edge) + "," + score + ",1)"
             lattice += lattice_edge + ","
         lattice = lattice[:len(lattice)-1] + "),"
     return lattice
 
-child = pexpect.spawn(
-    "java -Xmx4g -jar stagger/stagger.jar -modelfile swedish.bin "
-    "-posBeamSize 200 -multiple 10 -tag -")
-child.expect('Loaded')
 
-sys.stdout.write('(')
-sys.stdout.flush()
+def training_sent(stagger_analysis, include_minor=True):
+    stagger_analysis = proc_stagger_out(stagger_analysis)
+    sent_out = []
+    for line in stagger_analysis:
+        sent_out.append(line_to_factors(line, include_minor=include_minor))
+    return ' '.join(sent_out)
 
-# Input is in horizontal tokenized format, but stagger takes verical
-for line in sys.stdin:
-    for word in line.split(' '):
-        child.send(word + '\n')
-    child.send('\n')
-    child.expect('\r\n\r\n')
-    child.expect('\r\n\r\n\r\n')
-    sys.stdout.write(make_lattice(child.before))
+
+def start_stagger(multiple=None, beam_size=200):
+    global child
+    if multiple is not None:
+        mult_opt = "-multiple {} ".format(multiple)
+    else:
+        mult_opt = ""
+    child = pexpect.spawn(
+        ("java -Xmx4g -jar stagger/stagger.jar -modelfile swedish.bin "
+         "-posBeamSize {} {}-tag -").format(beam_size, mult_opt))
+    child.expect('Loaded')
+
+
+def stream_lattice(include_minor=True):
+    start_stagger(multiple=10)
+    sys.stdout.write('(')
     sys.stdout.flush()
 
-sys.stdout.write(')')
-sys.stdout.flush()
+    # Input is in horizontal tokenized format, but stagger takes verical
+    for line in sys.stdin:
+        for word in line.split(' '):
+            child.send(word + '\n')
+        child.send('\n')
+        child.expect('\r\n\r\n')
+        child.expect('\r\n\r\n\r\n')
+        sys.stdout.write(lattice_sent(
+            child.before, include_minor=include_minor))
+        sys.stdout.flush()
+
+    sys.stdout.write(')\n')
+    sys.stdout.flush()
+
+
+def stream_training(include_minor=True):
+    start_stagger()
+
+    # Input is in horizontal tokenized format, but stagger takes verical
+    for line in sys.stdin:
+        for word in line.split(' '):
+            child.send(word + '\n')
+        child.send('\n')
+        child.expect('\r\n\r\n')
+        child.expect('\r\n\r\n')
+        print(training_sent(
+            child.before, include_minor=include_minor))
+        sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    arg = sys.argv[1] if len(sys.argv) > 1 else ''
+    if arg == 'lattice':
+        stream_lattice()
+    elif arg == 'latticemaj':
+        stream_lattice(False)
+    elif arg == 'train':
+        stream_training()
+    elif arg == 'trainmaj':
+        stream_training(False)
+    else:
+        print("Usage {} lattice|latticemaj|train|trainmaj".format(sys.argv[0]))
