34d0465961182be7beb317f4c40b0802aa7d8d9e
Frankie Robertson <frankie@robertson.name>
2017-05-28 10:36:04 +0200

    PEP-8 format, remove newlines in output and flush, add major pos to last field, add quotes to first lattice field

--- tagging.py
+++ tagging.py
@@ -5,25 +5,24 @@ Need to have stagger.jar compiled under stagger directory (use ant)
 import sys
 import pexpect
 
-#with open('sample.txt') as stagger_analysis:
 
 def make_lattice(stagger_analysis):
-        
-    #Make a dictionary of all edges and their scores for one analysis
+    # Make a dictionary of all edges and their scores for one analysis
     analyses = {}
     stagger_analysis = stagger_analysis.decode("utf-8")
     stagger_analysis = stagger_analysis.splitlines()
     in_analysis = True
     keys = []
     to_sum = []
-    #Add new word analyses to the dictionary
+
+    # Add new word analyses to the dictionary
     for line in stagger_analysis:
         line = line.rstrip('\n')
         line = line.split('\t')
         if len(line) == 1:
             in_analysis = False
         if in_analysis:
-            line[5] = line[5].replace("|", ".")
+            line[5] = line[4] + "." + line[5].replace("|", ".")
             key = ''.join([line[1], "|", line[2], "|", line[4], "|", line[5]])
             to_sum.append(key)
             if key not in keys:
@@ -31,11 +30,12 @@ def make_lattice(stagger_analysis):
                 try:
                     analyses[line[0]][key] = 0
                 except:
-                    analyses[line[0]] = {key : 0}
-        #When reaching the result line, add it to corresponding words
+                    analyses[line[0]] = {key: 0}
+
+        # When reaching the result line, add it to corresponding words
         if len(line) == 1 and line != ['']:
             for word in to_sum:
-                for i in range(1,len(analyses)+1):
+                for i in range(1, len(analyses)+1):
                     i = str(i)
                     try:
                         analyses[i][word] += float(''.join(line))
@@ -44,28 +44,26 @@ def make_lattice(stagger_analysis):
             if len(to_sum) == len(analyses):
                 to_sum = []
         in_analysis = True
-    print(analyses)
 
-    #Create a lattice from the existing dictionary
-    lattice = "("
+    # Create a lattice from the existing dictionary
+    lattice = ""
     for i in range(1, len(analyses)+1):
         i = str(i)
         lattice += "("
         for edge in analyses[i]:
             score = str(analyses[i][edge])
-            lattice_edge = "(" + edge + "," + score + ",1)"
+            lattice_edge = "(" + repr(edge) + "," + score + ",1)"
             lattice += lattice_edge + ","
         lattice = lattice[:len(lattice)-1] + "),"
-    lattice = lattice[:len(lattice)-1] + ")"
-    print(lattice)
-    return lattice # TODO
+    return lattice
 
 child = pexpect.spawn(
     "java -Xmx4g -jar stagger/stagger.jar -modelfile swedish.bin "
     "-posBeamSize 200 -multiple 10 -tag -")
 child.expect('Loaded')
 
-print('(')
+sys.stdout.write('(')
+sys.stdout.flush()
 
 # Input is in horizontal tokenized format, but stagger takes verical
 for line in sys.stdin:
@@ -74,6 +72,8 @@ for line in sys.stdin:
     child.send('\n')
     child.expect('\r\n\r\n')
     child.expect('\r\n\r\n\r\n')
-    print(make_lattice(child.before))
+    sys.stdout.write(make_lattice(child.before))
+    sys.stdout.flush()
 
-print(')')
+sys.stdout.write(')')
+sys.stdout.flush()
